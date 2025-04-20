34cca9ee13358ccfca2cdd30ea76ce06a58163ed
Dizzzza <dias.svankulov@gmail.com>
2025-04-20 20:25:31 +0500

    k

--- auth-service/models/shipmentService.js
+++ auth-service/models/shipmentService.js
@@ -5,8 +5,14 @@ const SHIPMENT_URL = process.env.SHIPMENT_URL;
 
 // Получение списка компаний и поставщиков
 async function getEntities() {
+    const AUTH_TOKEN = process.env.JWT_TOKEN; // можешь заменить на переменную окружения или передавать параметром
+
     try {
-        const response = await axios.get(`${SHIPMENT_URL}/getEntities`);
+        const response = await axios.get(`${SHIPMENT_URL}/getEntities`, {
+            headers: {
+                Authorization: `Bearer ${AUTH_TOKEN}`
+            }
+        });
         return response.data.data;
     } catch (error) {
         console.error('Error fetching entities:', error.message);
@@ -16,8 +22,14 @@ async function getEntities() {
 
 // Добавление новой компании или поставщика
 async function addEntity(type, name) {
+    const AUTH_TOKEN = process.env.JWT_TOKEN;
+
     try {
-        const response = await axios.post(`${SHIPMENT_URL}/createEntity`, { type, name });
+        const response = await axios.post(`${SHIPMENT_URL}/createEntity`, { type, name }, {
+            headers: {
+                Authorization: `Bearer ${AUTH_TOKEN}`
+            }
+        });
         return response.data;
     } catch (error) {
         console.error(`Error adding ${type}:`, error.message);
